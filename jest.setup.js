982a5883f92947af23930f49bc47343bac050502
Sergey Andreev <sergey.and@okwork.io>
2024-10-22 14:37:26 +0300

    update: bump version react, react-dom

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+import util from "util"
+/* eslint-disable no-undef */
+Object.defineProperty(global, "TextEncoder", {
+  value: util.TextEncoder,
+})
--- tests/observer.test.js
+++ tests/observer.test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
+import Adapter from "@cfaester/enzyme-adapter-react-18"
 import Enzyme from "enzyme"
 
 import { observable } from "../src"
