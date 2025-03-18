d92a7ef6a3bf1cf5da6ad6a8c49a80a475752923
Alexander <153199358+alexander-baz@users.noreply.github.com>
2025-03-18 15:18:08 +0300

    Fix use store using stale data if change happened before useEffect hook (#19)

--- src/useStore.js
+++ src/useStore.js
@@ -3,7 +3,11 @@ import { useState, useEffect } from "react"
 const useStore = (store, { map } = {}) => {
   const [state, setState] = useState(store.getState())
 
-  useEffect(() => store.subscribe(data => setState(data)), [])
+  useEffect(() => {
+    const unsubscribe = store.subscribe(setState)
+    setState(store.getState())
+    return unsubscribe
+  }, [])
 
   return map ? map(state) : state
 }
